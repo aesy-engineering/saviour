318a1e3265e611cc6825632add34bf567e89c92f
ntinkster <nitin@getmho.com>
2023-12-12 13:05:40 +0530

    config changes

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ queries = [
     "Director Marketing",
     "Marketing Manager"
 ]
-number_of_results=3
+number_of_results=2
 delay_in_seconds = 2
-common_queries = "linkedIn"
+common_queries = "LinkedIn"
 
 final_base_query_list = ["{} {}".format(a, common_queries) for a in queries]
